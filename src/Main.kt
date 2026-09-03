dbf9042b91a860fc903fed247c786c0f971a6fec
Tori Nyakiamo <juliet.nyakiamo@strathmore.edu>
2026-09-03 18:40:28 +0300

    Starter

--- src/Main.kt
+++ src/Main.kt
@@ -1,79 +1,123 @@
+// Concept Note
+// 192789 Shali
+//200159  Juliet
+//175852 Kimberly
+//163034  Alvaros
+//189600 Elizabeth
+
+
 
 fun main() {
 
+    println("\n---------------------------------------------------")
+    println("Welcome to our Property Listings Management System")
+    println("-----------------------------------------------------\n")
 
-    //Tenant information
+// Landlord Details
 
-    println("_______________________________________")
-    println("Welcome to our Tenant Management System")
-    println("_______________________________________\n")
+    println("Landlord info: \n")
+    val landlordName: String = "Linda Chelagat"
+    val landlordId = 201
+    val landlordPhoneNo = "0767904538"
+    val landlordEmail = "lchelagat@gmail.com"
+    val propertyOwned =  "Sunrise Apartments"
+    var profileStatus = "Verified"
+
+
+    println("Landlord Name: $landlordName")
+    println("Landlord Id: $landlordId")
+    println("Phone Number: $landlordPhoneNo")
+    println("Email: $landlordEmail")
+    println("Property Owned: $propertyOwned")
+    println("Profile Status : $profileStatus")
+
 
     //Property  Details
 
-    println("Property 1 info \n")
+    println("\n Property info: \n")
     val propertyName: String = "Sunrise Apartments"
     val propertyId: Int = 3001
     val numberOfUnits = 6
     val propertyAddress: String = "Sun Road - 04506"
-    val landlordName = "Linda Chelagat"
-    val landlordId = 201
-    var unitStatus :String = ""
-    var hasTenant :Boolean = true
+    val propertyStatus = "Verified"
+    val currentTenants = 3
+    val monthlyRent: Int = 30000
 
-    if (hasTenant) {
-        unitStatus = "Assigned"
-    }else {
-        unitStatus = "Vacant"
 
-    }
 
     println("Property Name: $propertyName")
     println("Property ID: $propertyId")
     println("Number of Units : $numberOfUnits")
+    println("Number of Tenants $currentTenants")
     println("Property Address: $propertyAddress")
-    println("Landlord Name: $landlordName")
-    println("Landlord Id: $landlordId")
-    println("Unit Status: $unitStatus")
+    println("Property Status: $propertyStatus")
+    println("Monthly Rent: $monthlyRent")
 
 
-    //tenant 1
+    //Registration of new property listings
 
-    println("Tenants info \n")
-    val tenantNames = mutableListOf ("Yvonne Wambui","John Kirui", "Nina Almasi")
-    val tenantIds  = mutableListOf (101,102,103)
-    val houseNumbers = mutableListOf ("A-102","A-104","A-106")
-    val phoneNumbers = mutableListOf ("0780796245","0700385624","0790436875")
-    var tenantEmails = mutableListOf ("wambui@gmail.com"," kirui@gmail.com", "almasi@gmail.com")
-    var paymentModes = mutableListOf("credit card","M-pesa", "Cash")
-    val rentDueList = mutableListOf(40000,30000,25000)
+    println(" \n Enter Property name: \n ")
+    val newPropertyName = readln()
+    println("Enter Property ID:")
+    val newPropertyId = readln().toInt()
+    println("Enter Number Units:")
+    val newPropertyUnits = readln().toInt()
+    println("Enter Number Tenants:")
+    var numberOfTenants = readln().toInt()
+    println("Enter Property Address:")
+    val newPropertyAddress = readln()
+    println("Enter Property Status:")
+    var newPropertyStatus = readln()
+    println("Enter Property Monthly Rent:")
+    var newPropertyMonthlyRent = readln().toInt()
+
+println(" \n New Property Details: \n")
+    println("Property Name: $newPropertyName")
+    println("Property ID: $newPropertyId")
+    println("Number of Units : $newPropertyUnits")
+    println("Property Address: $newPropertyAddress")
+    println("Property Status: $newPropertyStatus")
+    println("Monthly Rent: $newPropertyMonthlyRent")
 
 
-    val availableUnits = numberOfUnits - tenantNames.size
 
-    for ((index, name) in tenantNames.withIndex()) {
-        println("Tenant Name: $tenantNames")
-        println("Tenant Id: $tenantIds[0]")
-        println("House Number: $houseNumbers[0]")
-        println("Phone Number: $phoneNumbers")
-        println("email: $tenantEmails")
-        println("mode of payment: $paymentModes")
-        println("Rent: $rentDueList")
 
-        println("key in amount paid")
-        var amountPaidList = readln() .toInt()
-        println("Amount: $amountPaidList")
 
-        var isRentPaidList: Boolean = amountPaidList >= rentDueList[index]
-        println("Tenant Paid : $isRentPaidList")
 
-        if (isRentPaidList) {
-            println("Thank you for the early rent pay")
-        } else {
-            println("please clear your balance {$rentDueList - $amountPaidList}")
-        }
-        println("Available Units: $availableUnits")
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 }
+
+
+
+
+
+
